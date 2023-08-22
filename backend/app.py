e60d13e45bb9b6a70820fa5bb2491e5663793c2a
dannyrojo <dannyred365+github@gmail.com>
2023-08-22 00:35:49 -0700

    we have connection

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/translate', methods=['POST'])
-def process_text():
-    data = request.json
-    if 'text' in data:
-        text_to_process = data['text']
-
+@app.route('/fetch_webpage', methods=['POST'])
+def fetch_webpage():
+    try:
+        data = request.json
+        if 'url' not in data:
+            return jsonify({'error': 'URL not provided'}), 400
+        
+        url = data['url']
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            return jsonify({'content': response.text}), 200
+        else:
+            return jsonify({'error': 'Failed to fetch webpage'}), response.status_code
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
